38edde21ea1dde6c400e746072bd2224d6cf1b3d
mrtolkien <gary.mialaret@gmail.com>
2022-12-30 15:16:02 +0900

    Added long content (3000 words) test data + wrote function

--- src/notion.rs
+++ src/notion.rs
@@ -23,6 +23,7 @@ struct NotionBookPage {
 // Creating our clip function that uses the parent
 impl BookClips {
     fn to_notion_body(&self, parent_page_id: String) -> NotionBookPage {
+        // TODO Use strongly typed JSON everywhere to make it easier, it's a bit disgusting here
         let mut children = Vec::new();
 
         children.push(json!({
@@ -51,30 +52,63 @@ impl BookClips {
         }));
 
         for clip in &self.clips {
-            children.push(json!({
-                "object": "block",
-                "type": "quote",
-                "quote": {
-                    "rich_text": [
-                        {
-                            "type": "text",
-                            "text": {
-                                "content": format!("{}\n", clip.content)
-                                }
-                        },
-                        {
-                            "type": "mention",
-                            "mention":{
-                                "type": "date",
-                                "date": {
-                                    "start": clip.date,
-                                }
+            let mut split_content = Vec::new();
+            let mut current_content = String::new();
+
+            // RE-CHECK WHY THIS IS NEEDED, 2000 characters is HUGE wtf
+            for phrase in clip.content.split_inclusive(". ") {
+                // We split every 1800 characters to leave space for the date
+                if current_content.len() + phrase.len() > 1800 {
+                    split_content.push(current_content);
+                    current_content = String::from(phrase);
+                // Else we just grow the content and add the dot back
+                } else {
+                    current_content.push_str(phrase);
+                }
+            }
+
+            // Adding the remainder (usually the whole content)
+            split_content.push(current_content);
+
+            // We iterate on content blocks
+            for (idx, content) in split_content.iter().enumerate() {
+                let quote_content = if idx < split_content.len() - 1 {
+                    // First part of quote: no line jump or date
+                    json!([{
+                        "type": "text",
+                        "text": {
+                            "content": format!("{}", content)
+                            }
+                    }])
+                } else {
+                    // Second part of quote: line jump and date
+                    json!([{
+                        "type": "text",
+                        "text": {
+                            "content": format!("{}\n", content)
+                            }
+                    },
+                    {
+                        "type": "mention",
+                        "mention":{
+                            "type": "date",
+                            "date": {
+                                "start": clip.date,
                             }
                         }
-                        ]
                     }
-                }
-            ))
+                    ])
+                };
+
+                children.push(json!({
+                    "object": "block",
+                    "type": "quote",
+                    "quote": {
+                        "rich_text": quote_content
+                        }
+                    }
+                ))
+            }
         }
 
         NotionBookPage {
