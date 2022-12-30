8f482488659601bd60f1501d00f2532b115c1634
mrtolkien <gary.mialaret@gmail.com>
2022-12-30 16:57:07 +0900

    Minimal main updates, merging

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,21 @@ fn main() {
     let clippings_text = fs::read_to_string(
         // TODO FIX THIS -> USE THE PROPER VALUE
         // env::var("CLIPPINGS_FILE").expect("CLIPPINGS_FILE env variable not set"),
-        "documents\\My Clippings.txt",
+        "documents/My Clippings.txt",
     )
-    .expect("Test file not found");
-    let clips = clippings::parse_clips(clippings_text.as_str());
+    .expect("Clippings file not found");
+
+    let books_clips = clippings::parse_clips(clippings_text.as_str());
 
     // Reading the environment variables
     let api_key = env::var("NOTION_API_KEY").expect("NOTION_API_KEY env variable not set");
     let parent_page_id = env::var("NOTION_PAGE_ID").expect("NOTION_PAGE_ID env variable not set");
 
-    println!("STARTING UPLOAD of {} clips", clips.len());
+    println!("STARTING UPLOAD of {} books", books_clips.len());
 
     // Uploading to Notion
-    notion::upload_to_notion(api_key, parent_page_id, clips).expect("Failed to upload to Notion");
+    notion::upload_to_notion(api_key, parent_page_id, books_clips)
+        .expect("Failed to upload to Notion");
 
     println!("ENDING UPLOAD");
 }
