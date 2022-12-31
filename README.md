2456bf86c858a2d445a0a7743c64ee33e0ca693e
mrtolkien <gary.mialaret@gmail.com>
2022-12-31 13:59:48 +0900

    Better main function + README

--- README.md
+++ README.md
@@ -9,4 +9,15 @@ NOTION_API_KEY=...
 NOTION_PAGE_ID=...
 ```
 
-<!-- TODO Add releases + how to use! -->
+## Usage
+
+- Create a Notion API "Internal integration" at <https://www.notion.so/my-integrations> with read, update, and insert content capabilities
+- Download and save the [latest release binary from the releases page](https://github.com/mrtolkien/kindle_to_notion/releases) to the root of your Kindle
+- Create a `.env` file at the root of your Kindle with the same structure as [this example `.env` file](https://github.com/mrtolkien/kindle_to_notion/blob/main/.env.example)
+  - `NOTION_API_KEY` is the API key for your integration
+  - `NOTION_PAGE_ID` is the page ID of the page where you want to insert your clippings pages
+- Run the executable from the root of your Kindle and see it populate
+
+## Behaviour and limitations
+
+- TODO
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,27 @@
 use dotenvy;
+use std::path::PathBuf;
 use std::{env, fs};
 
 use kindle_to_notion::{clippings, notion};
 
 fn main() {
-    // TODO Get config from file instead? inspo from: https://github.com/jakeswenson/notion/blob/main/examples/todo/main.rs
-    dotenvy::dotenv().unwrap();
+    // For simplicity, the conf is read from a .env file at the moment
+    dotenvy::dotenv().expect(".env file not found");
 
     // Reading the clippings
-    let clippings_text = fs::read_to_string(
-        // TODO FIX THIS -> USE THE PROPER VALUE
-        // env::var("CLIPPINGS_FILE").expect("CLIPPINGS_FILE env variable not set"),
-        "documents\\My Clippings.txt",
-    )
-    .expect("Test file not found");
+    // TODO Add a CLI option to specify the clippings file
+    let file_path: PathBuf = ["documents", "My Clippings.txt"].iter().collect();
+    let clippings_text = fs::read_to_string(file_path).expect("{file_path} file not found");
+
+    // Creating our clips data
     let clips = clippings::parse_clips(clippings_text.as_str());
 
-    // Reading the environment variables
+    // Reading the environment variables for Notion
     let api_key = env::var("NOTION_API_KEY").expect("NOTION_API_KEY env variable not set");
     let parent_page_id = env::var("NOTION_PAGE_ID").expect("NOTION_PAGE_ID env variable not set");
-
-    println!("STARTING UPLOAD of {} clips", clips.len());
 
     // Uploading to Notion
     notion::upload_to_notion(api_key, parent_page_id, clips).expect("Failed to upload to Notion");
 
-    println!("ENDING UPLOAD");
+    // TODO Add a CLI option to archive the clippings
 }
